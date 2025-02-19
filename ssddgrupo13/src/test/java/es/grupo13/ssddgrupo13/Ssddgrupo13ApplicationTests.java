bf779ff269e68f79219c2986dac5bee8bf1282e8
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-02-19 13:11:41 +0100

    Meti el Spring

---
+++ ssddgrupo13/src/test/java/es/grupo13/ssddgrupo13/Ssddgrupo13ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.grupo13.ssddgrupo13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssddgrupo13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
