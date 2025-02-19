85ebb4c66023419bbd846f8fa161686e1f052bcd
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-02-19 19:36:40 +0100

    Separacion secciones

--- ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/ControllerApp.java
+++ ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/ControllerApp.java
@@ -10,6 +10,22 @@ public class ControllerApp {
     public String homeLink() {
         return "section_1";
     }
-
+    @GetMapping("/section_2")
+    public String clubbingLink() {
+        return "section_2";
+    }
+    @GetMapping("/section_3")
+    public String conciertosLink() {
+        return "section_3";
+    }
+    @GetMapping("/section_4")
+    public String eventosLink() {
+        return "section_4";
+    }
+    @GetMapping("/section_5")
+    public String contactanosLink() {
+        return "section_5";
+    }
+    
 
 }
