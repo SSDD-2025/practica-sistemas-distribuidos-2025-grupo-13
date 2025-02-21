4d9c57184f441e95e8919d0e59a940b967a43b1f
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-02-21 11:07:37 +0100

    Mapping

--- ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/ControllerApp.java
+++ ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/ControllerApp.java
@@ -1,35 +1,34 @@
 package es.grupo13.ssddgrupo13;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 public class ControllerApp {
 
-    @PostMapping("/home")
+    @GetMapping("/home")
     public String homeLink() {
         return "../Static/index";
     }
-    @PostMapping("/clubbing")
+    @GetMapping("/clubbing")
     public String clubbingLink() {
         return "section_2";
     }
-    @PostMapping("/conciertos")
+    @GetMapping("/conciertos")
     public String conciertosLink() {
         return "section_3";
     }
-    @PostMapping("/eventos")
+    @GetMapping("/eventos")
     public String eventosLink() {
         return "section_4";
     }
-    @PostMapping("/contactanos")
+    @GetMapping("/contactanos")
     public String contactanosLink() {
         return "section_5";
     }
-    @PostMapping("/error")
-    public String getMethodName(@RequestParam String param) {
+    @GetMapping("/error")
+    public String errorLink() {
         return "error";
     }
     
