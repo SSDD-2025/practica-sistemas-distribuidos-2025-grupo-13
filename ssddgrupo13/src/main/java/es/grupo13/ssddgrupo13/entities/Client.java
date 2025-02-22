3b2ca13bc6f7d0b3e2a1805c492e3c0bfb306346
Robert Gabriel Mihai <roby07g@gmail.com>
2025-02-22 18:14:06 +0100

    Creados atributos de la entidad Cliente y entidad Comment completada

--- ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/entities/Client.java
+++ ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/entities/Client.java
@@ -1,5 +1,25 @@
 package es.grupo13.ssddgrupo13.entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
 public class Client {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private String surname;
+    private String password;
+
+    @Email(message = "The email has to be valid")
+    @NotBlank(message = "The email cannot be empty")
+    @Column(unique = true, nullable = false)
+    private String email;
+    
+    
+
 }
--- ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/entities/Comment.java
+++ ssddgrupo13/src/main/java/es/grupo13/ssddgrupo13/entities/Comment.java
@@ -1,5 +1,49 @@
 package es.grupo13.ssddgrupo13.entities;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Comment {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String text;
+    private int rating;
+
+    protected Comment() {
+
+    }
+
+    public Comment(String text, int rating) {
+        super();
+        this.text = text;
+        this.rating = rating;
+    }
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
 }
