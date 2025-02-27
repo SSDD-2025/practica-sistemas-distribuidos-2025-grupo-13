f80d518a7a6c19c004c190c7c8c0f683df0b3011
Robert Gabriel Mihai <roby07g@gmail.com>
2025-02-28 00:01:25 +0100

    empezado html y css para enlace Mis Datos, contienen la lista de tickets y comentarios del usuario que ha iniciado sesion

--- src/main/java/es/grupo13/ssddgrupo13/controller/TicketController.java
+++ src/main/java/es/grupo13/ssddgrupo13/controller/TicketController.java
@@ -1,10 +1,5 @@
 package es.grupo13.ssddgrupo13.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
-import es.grupo13.ssddgrupo13.repository.TicketRepository;
-import jakarta.annotation.PostConstruct;
-
 public class TicketController {
 
 
