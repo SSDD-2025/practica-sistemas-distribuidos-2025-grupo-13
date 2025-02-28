ce9d442f35964173bfee4c2df482a840461235e1
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-02-28 19:50:53 +0100

    Cerrar sesion

--- src/main/java/es/grupo13/ssddgrupo13/controller/ClientController.java
+++ src/main/java/es/grupo13/ssddgrupo13/controller/ClientController.java
@@ -6,20 +6,18 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.grupo13.ssddgrupo13.entities.Client;
 import es.grupo13.ssddgrupo13.entities.Comment;
-import es.grupo13.ssddgrupo13.entities.Event;
 import es.grupo13.ssddgrupo13.entities.Ticket;
 import es.grupo13.ssddgrupo13.repository.ClientRepository;
 import es.grupo13.ssddgrupo13.repository.CommentRepository;
 import es.grupo13.ssddgrupo13.repository.TicketRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 
@@ -33,7 +31,8 @@ public class ClientController {
 
     @Autowired
     private CommentRepository commentRepository;
-
+    
+    private Boolean isLogged = false;
     
 	@PostConstruct
 	public void init() {
@@ -68,6 +67,7 @@ public class ClientController {
         System.out.println(clientLogin.get().getEmail());
         if (clientLogin.isPresent()) {
             session.setAttribute("client", clientLogin.get());
+            isLogged = true;
             return "/inicioSesion";
         } else {
             return "/error";
@@ -119,6 +119,20 @@ public class ClientController {
         model.addAttribute("tickets", tickets);
         model.addAttribute("comments", comments);
         return "misdatos";
+    }
+    @GetMapping("/profile")
+    public String profileLink(HttpSession session, Model model) {
+        if(!isLogged)return "/profile";
+        else{
+            model.addAttribute("client", session.getAttribute("client"));
+             return "/profile_out";
+        }
+    }
+    @PostMapping("/log_out")
+    public String logout(HttpSession session) {
+        isLogged = false;
+        session.invalidate();
+        return "/index";
     }
     
 
