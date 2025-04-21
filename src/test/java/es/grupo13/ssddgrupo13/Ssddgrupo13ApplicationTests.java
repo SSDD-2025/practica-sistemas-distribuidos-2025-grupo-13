6accc55df1baccd8ba978888a8d4d503c0a94295
Álvaro Osuna Flores <a.osuna.2022@alumnos.urjc.es>
2025-04-21 12:23:49 +0200

    Revert "update: api-doc config"
    This reverts commit dfac66bec5b6fe507418b668078b99f65e11e016.

--- src/test/java/es/grupo13/ssddgrupo13/Ssddgrupo13ApplicationTests.java
+++ src/test/java/es/grupo13/ssddgrupo13/Ssddgrupo13ApplicationTests.java
@@ -3,8 +3,7 @@ package es.grupo13.ssddgrupo13;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@SpringBootTest
 class Ssddgrupo13ApplicationTests {
 
 	@Test
