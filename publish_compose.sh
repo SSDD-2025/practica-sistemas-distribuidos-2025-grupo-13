71ad34e153db099bc45b49173831056ec2b47a50
AlvaroOsunaFlores <a.osuna.2022@alumnos.urjc.es>
2025-05-19 19:33:09 +0200

    Update publish_compose.sh

--- publish_compose.sh
+++ publish_compose.sh
@@ -5,10 +5,6 @@ set -e
 
 COMPOSE_FILES="-f docker-compose.local.yml -f docker-compose.prod.yml"
 
-
-docker compose $COMPOSE_FILES build
-
-
-docker compose $COMPOSE_FILES push
+docker compose $COMPOSE_FILES push jonmazzh/grupo13:1.0.1
 
 
